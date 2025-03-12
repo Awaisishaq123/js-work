44b6e4bc9ea07960be56c583aee7e87c4733b816
Awaisishaq123 <awaisishaq003@gmail.com>
2025-03-12 11:32:02 +0000

    initail'

---
+++ class 14/script.js
@@ -0,0 +1,17 @@
+// let str = "a,e,i,o,u"
+// let count = 0;
+// for (let i=0; i<str.length; i++){
+//     if(str[i]==="a"||str[i]==="e"|| str[i]==="i"|| str[i]==="o"||str[i]==="u"){
+//          count++;
+//     }
+//     document.write(count)
+// }
+
+
+
+
+// let num = [1,2,3,4,5,6,7,8,9]
+// for(let i=0;i<num.length; i++){
+//     num[i] =num[i]*num[i]
+// }
+// document.write(num)
